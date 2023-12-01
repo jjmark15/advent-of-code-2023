5304056bb6644f0046023097ab6cf9ad6749da9a
Josh Jones <ohblonddev@gmail.com>
2023-12-01 21:12:33 +0000

    Solve day 1 part 2

--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -5,23 +5,46 @@ import strikt.assertions.isEqualTo
 class Day1Test {
     @Test
     fun `part 1 long`() {
-        expectThat(sumOfCalibrationValues(longData())) isEqualTo 54877
+        expectThat(naiveSumOfCalibrationValues(longData())) isEqualTo 54877
     }
 
     @Test
     fun `part 1 short`() {
-        expectThat(sumOfCalibrationValues(shortData())) isEqualTo 142
+        expectThat(naiveSumOfCalibrationValues(shortData())) isEqualTo 142
     }
 
-//    @Test
-//    fun `part 2 long`() {
-//        expectThat(totalCaloriesCarriedByTopThreeElves(longData())) isEqualTo 206152
-//    }
-//
-//    @Test
-//    fun `part 2 short`() {
-//        expectThat(totalCaloriesCarriedByTopThreeElves(shortData())) isEqualTo 45000
-//    }
+    @Test
+    fun `part 2 long`() {
+        expectThat(sumOfCalibrationValues(longData())) isEqualTo 54100
+    }
+
+    @Test
+    fun `part 2 short`() {
+        expectThat(
+            sumOfCalibrationValues(
+                listOf(
+                    "two1nine",
+                    "eightwothree",
+                    "abcone2threexyz",
+                    "xtwone3four",
+                    "4nineeightseven2",
+                    "zoneight234",
+                    "7pqrstsixteen",
+                )
+            )
+        ) isEqualTo 281
+    }
+
+    @Test
+    fun `part 2 cares about first and last words`() {
+        expectThat(
+            sumOfCalibrationValues(
+                listOf(
+                    "twone",
+                )
+            )
+        ) isEqualTo 21
+    }
 
     private fun longData(modifier: String? = null) = loadData(1, modifier)
 
