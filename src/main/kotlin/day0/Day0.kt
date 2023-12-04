78fd191603c3e57dac34373c45dd96fff00aaa69
Josh Jones <ohblonddev@gmail.com>
2023-12-04 17:04:12 +0000

    Delete demo day

--- src/main/kotlin/day0/Day0.kt
+++
@@ -1,6 +0,0 @@
-package day0
-
-fun maxCaloriesCarried(calories: List<List<Int>>): Int = calories.maxOf(List<Int>::sum)
-
-fun totalCaloriesCarriedByTopThreeElves(calories: List<List<Int>>): Int =
-    calories.map(List<Int>::sum).sorted().takeLast(3).sum()
