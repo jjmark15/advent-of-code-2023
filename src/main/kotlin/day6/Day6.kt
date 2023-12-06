286cd004c1843ec51f6c5e2c3fb205777a12a158
Josh Jones <ohblonddev@gmail.com>
2023-12-06 12:39:28 +0000

    Solve day 6 part 2

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -1,11 +1,11 @@
 package day6
 
 fun part1(records: List<BoatRecord>): Int = records.map { (recordDistance, recordTime) ->
-    (0..recordTime).count { chargingTime ->
+    (0L..recordTime).count { chargingTime ->
         val distance = chargingTime * (recordTime - chargingTime)
         distance > recordDistance
     }
 }.reduce { acc, i -> acc * i }
 
 
-data class BoatRecord(val distance: Int, val time: Int)
+data class BoatRecord(val distance: Long, val time: Long)
--- src/test/kotlin/day6/Day6Test.kt
+++ src/test/kotlin/day6/Day6Test.kt
@@ -9,21 +9,37 @@ import java.util.regex.Pattern
 class Day6Test {
     @Test
     fun `part 1 long`() {
-        expectThat(part1(data())) isEqualTo 2449062
+        expectThat(part1(parse1(data()))) isEqualTo 2449062
     }
 
     @Test
     fun `part 1 short`() {
-        expectThat(part1(shortData())) isEqualTo 288
+        expectThat(part1(parse1(shortData()))) isEqualTo 288
     }
 
-    private fun data(modifier: String? = null) = parse(loadData(6, modifier))
+    @Test
+    fun `part 2 long`() {
+        expectThat(part1(parse2(data()))) isEqualTo 33149631
+    }
+
+    @Test
+    fun `part 2 short`() {
+        expectThat(part1(parse2(shortData()))) isEqualTo 71503
+    }
+
+    private fun data(modifier: String? = null) = loadData(6, modifier)
 
     private fun shortData() = data("short")
 
-    private fun parse(lines: List<String>): List<BoatRecord> {
-        val times = lines[0].removePrefix("Time:").trim().split(Pattern.compile("\\s+")).map { it.toInt() }
-        val distances = lines[1].removePrefix("Distance:").trim().split(Pattern.compile("\\s+")).map { it.toInt() }
+    private fun parse1(lines: List<String>): List<BoatRecord> {
+        val times = lines[0].removePrefix("Time:").trim().split(Pattern.compile("\\s+")).map { it.toLong() }
+        val distances = lines[1].removePrefix("Distance:").trim().split(Pattern.compile("\\s+")).map { it.toLong() }
         return times.zip(distances).map { (time, distance) -> BoatRecord(distance, time) }
+    }
+
+    private fun parse2(lines: List<String>): List<BoatRecord> {
+        val time = lines[0].removePrefix("Time:").replace(" ", "").toLong()
+        val distance = lines[1].removePrefix("Distance:").replace(" ", "").toLong()
+        return listOf(BoatRecord(distance, time))
     }
 }
