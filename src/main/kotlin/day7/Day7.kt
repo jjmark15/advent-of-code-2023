7236266e51951287c2dde8b3b2e0eff40d28b3a5
Josh Jones <ohblonddev@gmail.com>
2023-12-07 23:32:22 +0000

    Solve day 7 part 2

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -21,10 +21,29 @@ sealed class HandType : Comparable<HandType> {
 
 data class Hand(val cards: List<CamelCard>) : Comparable<Hand> {
     private fun type(): HandType {
-        val counts = cards.fold(mutableMapOf<CamelCard, Int>()) { acc, camelCard ->
+        val jokers = cards.count { it.value == 'X' }
+        var counts = cards.filter { it.value != 'X' }.fold(mutableMapOf<CamelCard, Int>()) { acc, camelCard ->
             acc.compute(camelCard) { _, current -> (current ?: 0) + 1 }
             acc
-        }.values.sorted().reversed()
+        }.values.sorted().reversed().toMutableList()
+
+        repeat(jokers) {
+            var i = 0
+            while (true) {
+                val atIndex = counts.getOrElse(i) { 0 } + 1
+                val newList = counts.toMutableList()
+                if (newList.size == i) {
+                    newList.add(atIndex)
+                }
+                newList[i] = atIndex
+                val newListTotal = newList.sum()
+                if (newListTotal <= 5) {
+                    counts = newList
+                    break
+                }
+                i++
+            }
+        }
 
         return when (counts) {
             listOf(5) -> HandType.FiveOfAKind
@@ -56,7 +75,7 @@ data class Hand(val cards: List<CamelCard>) : Comparable<Hand> {
 
 data class CamelCard(val value: Char) : Comparable<CamelCard> {
     override fun compareTo(other: CamelCard): Int {
-        val order = "AKQJT987654321".reversed()
+        val order = "AKQJT987654321X".reversed()
 
         return order.indexOf(this.value) - order.indexOf(other.value)
     }
--- src/test/kotlin/day7/Day7Test.kt
+++ src/test/kotlin/day7/Day7Test.kt
@@ -8,15 +8,25 @@ import strikt.assertions.isEqualTo
 class Day7Test {
     @Test
     fun `part 1 long`() {
-        expectThat(part1(data())) isEqualTo 247823654
+        expectThat(part1(data().map { parse1(it) })) isEqualTo 247823654
     }
 
     @Test
     fun `part 1 short`() {
-        expectThat(part1(shortData())) isEqualTo 6440
+        expectThat(part1(shortData().map { parse1(it) })) isEqualTo 6440
     }
 
-    private fun data(modifier: String? = null) = loadData(7, modifier).map { parse(it) }
+    @Test
+    fun `part 2 long`() {
+        expectThat(part1(data().map { parse2(it) })) isEqualTo 245461700
+    }
+
+    @Test
+    fun `part 2 short`() {
+        expectThat(part1(shortData().map { parse2(it) })) isEqualTo 5905
+    }
+
+    private fun data(modifier: String? = null) = loadData(7, modifier)
 
     private fun shortData() = data("short")
 }
