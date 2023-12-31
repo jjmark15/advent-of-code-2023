48cf92d56810397a24be7266863680f31db82d98
Josh Jones <ohblonddev@gmail.com>
2023-12-31 23:27:01 +0000

    Solve day 9 part 2

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -2,6 +2,8 @@ package day9
 
 fun part1(input: List<List<Int>>): Int = input.sumOf { predictedNextValue(it) }
 
+fun part2(input: List<List<Int>>): Int = input.sumOf { predictedPreviousValue(it) }
+
 fun predictedNextValue(sequence: List<Int>): Int {
     var differencesEqualZero = false
     var previousSequence = sequence
@@ -16,4 +18,20 @@ fun predictedNextValue(sequence: List<Int>): Int {
     }
 
     return lastKnownValues.reversed().reduce { acc, i -> acc + i }
+}
+
+fun predictedPreviousValue(sequence: List<Int>): Int {
+    var differencesEqualZero = false
+    var previousSequence = sequence
+    val firstKnownValues = mutableListOf(sequence.first())
+
+    while (!differencesEqualZero) {
+        val differences = previousSequence.windowed(2).map { (a, b) -> b - a }
+
+        if (differences.all { it == 0 }) differencesEqualZero = true
+        previousSequence = differences
+        firstKnownValues.add(differences.first())
+    }
+
+    return firstKnownValues.reversed().reduce { acc, i -> i - acc }
 }
--- src/test/kotlin/day9/Day9Test.kt
+++ src/test/kotlin/day9/Day9Test.kt
@@ -16,6 +16,15 @@ class Day9Test {
     fun `part 1 short`() {
         expectThat(part1(data("short"))) isEqualTo 114
     }
+    @Test
+    fun `part 2 long`() {
+        expectThat(part2(data())) isEqualTo 1131
+    }
+
+    @Test
+    fun `part 2 short`() {
+        expectThat(part2(data("short"))) isEqualTo 2
+    }
 
     private fun data(modifier: String? = null) = loadData(9, modifier).map {
         it.split(Pattern.compile("\\s+")).filterNot { it.isBlank() }.map { it.toInt() }
