700863f869bcb8ca9288b55c3c9c6a823e5d0138
Josh Jones <ohblonddev@gmail.com>
2024-01-02 23:20:10 +0000

    Refactor

--- src/main/kotlin/day10/Parsing.kt
+++ src/main/kotlin/day10/Parsing.kt
@@ -1,19 +1,18 @@
 package day10
 
-fun parse(lines: List<String>): PipeGrid {
-    return PipeGrid(lines.map { line ->
-        line.split("").filter { it.isNotEmpty() }.map { character ->
-            when (character) {
-                "." -> GridElement.Ground
-                "S" -> GridElement.StartingPosition
-                "|" -> PipePiece.Vertical
-                "-" -> PipePiece.Horizontal
-                "L" -> PipePiece.NorthEast
-                "J" -> PipePiece.NorthWest
-                "7" -> PipePiece.SouthWest
-                "F" -> PipePiece.SouthEast
-                else -> throw Exception("could not parse $character")
-            }
-        }
-    })
-}
+import utils.map2D
+import utils.to2DCharacterMatrix
+
+fun parse(lines: List<String>): PipeGrid = lines.to2DCharacterMatrix().map2D { character ->
+    when (character) {
+        "." -> GridElement.Ground
+        "S" -> GridElement.StartingPosition
+        "|" -> PipePiece.Vertical
+        "-" -> PipePiece.Horizontal
+        "L" -> PipePiece.NorthEast
+        "J" -> PipePiece.NorthWest
+        "7" -> PipePiece.SouthWest
+        "F" -> PipePiece.SouthEast
+        else -> throw Exception("could not parse $character")
+    }
+}.let { PipeGrid(it) }
--- src/main/kotlin/utils/ListUtils.kt
+++ src/main/kotlin/utils/ListUtils.kt
@@ -9,3 +9,9 @@ fun List<String>.lineGroups(): List<List<String>> =
         }
         lineGroups
     }.filter { it.isNotEmpty() }
+
+fun <T, U> List<List<T>>.map2D(convert: (T) -> U): List<List<U>> = this.map { row -> row.map(convert) }
+
+fun List<String>.to2DCharacterMatrix(): List<List<String>> {
+    return this.map { row -> row.split("").filter { it.isNotEmpty() } }
+}
