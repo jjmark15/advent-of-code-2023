eca2e3508ccf2a8176fd7bbe03803ff6eeb5edf2
Josh Jones <ohblonddev@gmail.com>
2024-11-21 21:46:03 +0000

    Update Kotlin and dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.0.21"
 }
 
 group = "clumsywizard"
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.kittinunf.result:result:5.3.0")
-    implementation(platform("dev.forkhandles:forkhandles-bom:2.3.0.0"))
+    implementation("com.github.kittinunf.result:result:5.6.0")
+    implementation(platform("dev.forkhandles:forkhandles-bom:2.20.0.0"))
     implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
     testImplementation(kotlin("test"))
     testImplementation("io.strikt:strikt-core:0.34.0")
