2bc370b64842ce77954d3c29deac9e48a1108cca
Josh Jones <ohblonddev@gmail.com>
2024-12-01 18:36:57 +0000

    Refactor

--- src/test/kotlin/twenty_four/day1/Day1Test.kt
+++ src/test/kotlin/twenty_four/day1/Day1Test.kt
@@ -1,31 +1,31 @@
 package twenty_four.day1
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import loadData
 import org.junit.jupiter.api.Test
-import strikt.api.expectThat
-import strikt.assertions.isEqualTo
 
 class Day1Test {
     private val parser = InputParser()
 
     @Test
     fun `part 1 long`() {
-        expectThat(part1(parser.parse(data()))) isEqualTo 2430334
+        assertThat(part1(parser.parse(data()))).isEqualTo(2430334)
     }
 
     @Test
     fun `part 1 short`() {
-        expectThat(part1(parser.parse(shortData()))) isEqualTo 11
+        assertThat(part1(parser.parse(shortData()))).isEqualTo(11)
     }
 
     @Test
     fun `part 2 long`() {
-        expectThat(part2(parser.parse(data()))) isEqualTo 28786472
+        assertThat(part2(parser.parse(data()))).isEqualTo(28786472)
     }
 
     @Test
     fun `part 2 short`() {
-        expectThat(part2(parser.parse(shortData()))) isEqualTo 31
+        assertThat(part2(parser.parse(shortData()))).isEqualTo(31)
     }
 
     private fun data(modifier: String? = null) = loadData(2024, 1, modifier)
