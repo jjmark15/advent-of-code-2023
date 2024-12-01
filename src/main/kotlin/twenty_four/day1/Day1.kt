bc09c7a6743c3a7e0a6a5062db46a2f5e492cd1f
Josh Jones <ohblonddev@gmail.com>
2024-12-01 18:31:37 +0000

    Solve day 1 part 2

--- src/main/kotlin/twenty_four/day1/Day1.kt
+++ src/main/kotlin/twenty_four/day1/Day1.kt
@@ -9,3 +9,12 @@ fun part1(input: LocationIds): Long {
     val second = input.right.sorted()
     return first.zip(second).sumOf { (l, r) -> (l - r).absoluteValue }
 }
+
+fun part2(input: LocationIds): Long {
+    val cache = mutableMapOf<Long, Long>()
+
+    val numbers = input.left
+    val reference = input.right.sorted()
+
+    return numbers.sumOf { number -> number * cache.getOrPut(number) { reference.count { it == number }.toLong() } }
+}
--- src/test/kotlin/twenty_four/day1/Day1Test.kt
+++ src/test/kotlin/twenty_four/day1/Day1Test.kt
@@ -18,6 +18,16 @@ class Day1Test {
         expectThat(part1(parser.parsePart1(shortData()))) isEqualTo 11
     }
 
+    @Test
+    fun `part 2 long`() {
+        expectThat(part2(parser.parsePart1(data()))) isEqualTo 28786472
+    }
+
+    @Test
+    fun `part 2 short`() {
+        expectThat(part2(parser.parsePart1(shortData()))) isEqualTo 31
+    }
+
     private fun data(modifier: String? = null) = loadData(2024, 1, modifier)
 
     private fun shortData() = data("short")
