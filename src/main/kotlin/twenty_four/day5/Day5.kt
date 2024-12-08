33cfed372041b237edd4db5ba69681f233921fbe
Josh Jones <ohblonddev@gmail.com>
2024-12-08 21:41:09 +0000

    Minor refactor

--- src/main/kotlin/twenty_four/day5/Day5.kt
+++ src/main/kotlin/twenty_four/day5/Day5.kt
@@ -11,28 +11,24 @@ data class UpdatePages(val pages: List<Int>) {
 data class PuzzleInput(val rules: List<PageOrderingRule>, val updates: List<UpdatePages>)
 
 private class PageComparator(private val rules: List<PageOrderingRule>) : Comparator<Int> {
-    override fun compare(before: Int, after: Int): Int {
-        val rule =
-            rules.find { rule -> rule == PageOrderingRule(before, after) || rule == PageOrderingRule(before, after) }
-
-        if (rule == null) return 0
-        if (rule.after == after) return -1
-        return 1
-    }
+    override fun compare(before: Int, after: Int): Int =
+        rules.find { rule -> rule == PageOrderingRule(before, after) || rule == PageOrderingRule(before, after) }
+            ?.let { rule ->
+                if (rule.after == after) return@let -1
+                1
+            } ?: 0
 }
 
 fun part1(input: PuzzleInput): Int {
     val pageComparator = PageComparator(input.rules)
-    return input.updates.filter { pages -> pages.sortedWith(pageComparator) == pages }
-        .sumOf { it.middlePage() }
+    return input.updates.filter { pages -> pages.sortedWith(pageComparator) == pages }.sumOf { it.middlePage() }
 }
 
 fun part2(input: PuzzleInput): Int {
     val pageComparator = PageComparator(input.rules)
-    val middlePages: List<Int> = input.updates.mapNotNull { pages ->
+    return input.updates.sumOf { pages ->
         val sorted = pages.sortedWith(pageComparator)
-        if (sorted == pages) return@mapNotNull null
+        if (sorted == pages) return@sumOf 0
         sorted.middlePage()
     }
-    return middlePages.sum()
 }
