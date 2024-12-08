59f97da6edc71a6fa3e9a3563e663e602e6ac795
Josh Jones <ohblonddev@gmail.com>
2024-12-08 21:35:24 +0000

    Solve day 5 part 2

--- src/main/kotlin/twenty_four/day5/Day5.kt
+++ src/main/kotlin/twenty_four/day5/Day5.kt
@@ -4,16 +4,35 @@ data class PageOrderingRule(val before: Int, val after: Int)
 
 data class UpdatePages(val pages: List<Int>) {
     fun middlePage(): Int = pages[(pages.size - 1) / 2]
-}
 
-fun UpdatePages.satisfiesAllOf(rules: List<PageOrderingRule>): Boolean = rules.all { rule ->
-    val beforeIndex = pages.indexOf(rule.before)
-    val afterIndex = pages.indexOf(rule.after)
-    if (beforeIndex == -1 || afterIndex == -1) return@all true
-    beforeIndex < afterIndex
+    fun sortedWith(comparator: Comparator<Int>): UpdatePages = UpdatePages(pages.sortedWith(comparator))
 }
 
 data class PuzzleInput(val rules: List<PageOrderingRule>, val updates: List<UpdatePages>)
 
-fun part1(input: PuzzleInput): Int =
-    input.updates.filter { pages -> pages.satisfiesAllOf(input.rules) }.sumOf { it.middlePage() }
+private class PageComparator(private val rules: List<PageOrderingRule>) : Comparator<Int> {
+    override fun compare(before: Int, after: Int): Int {
+        val rule =
+            rules.find { rule -> rule == PageOrderingRule(before, after) || rule == PageOrderingRule(before, after) }
+
+        if (rule == null) return 0
+        if (rule.after == after) return -1
+        return 1
+    }
+}
+
+fun part1(input: PuzzleInput): Int {
+    val pageComparator = PageComparator(input.rules)
+    return input.updates.filter { pages -> pages.sortedWith(pageComparator) == pages }
+        .sumOf { it.middlePage() }
+}
+
+fun part2(input: PuzzleInput): Int {
+    val pageComparator = PageComparator(input.rules)
+    val middlePages: List<Int> = input.updates.mapNotNull { pages ->
+        val sorted = pages.sortedWith(pageComparator)
+        if (sorted == pages) return@mapNotNull null
+        sorted.middlePage()
+    }
+    return middlePages.sum()
+}
--- src/test/kotlin/twenty_four/day5/Day5Test.kt
+++ src/test/kotlin/twenty_four/day5/Day5Test.kt
@@ -18,6 +18,16 @@ class Day5Test {
         assertThat(part1(parser.parse(shortData()))).isEqualTo(143)
     }
 
+    @Test
+    fun `part 2 long`() {
+        assertThat(part2(parser.parse(data()))).isEqualTo(4151)
+    }
+
+    @Test
+    fun `part 2 short`() {
+        assertThat(part2(parser.parse(shortData()))).isEqualTo(123)
+    }
+
     private fun data(modifier: String? = null) = loadData(2024, 5, modifier)
 
     private fun shortData() = data("short")
