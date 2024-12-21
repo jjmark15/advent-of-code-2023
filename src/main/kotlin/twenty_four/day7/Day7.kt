7b06d097328c9e148bdb0eae2ce14da2cb9be246
Josh Jones <ohblonddev@gmail.com>
2024-12-21 14:39:35 +0000

    Solve day 7 part 2

--- src/main/kotlin/twenty_four/day7/Day7.kt
+++ src/main/kotlin/twenty_four/day7/Day7.kt
@@ -1,14 +1,34 @@
 package twenty_four.day7
 
+import java.util.function.Function
+
 data class Equation(val testValue: Long, val numbers: List<Long>)
 
 private enum class Operator {
-    Plus, Times
+    Plus, Times, Concat
 }
 
-fun part1(input: List<Equation>): Long = input.filter { canBeCombined(it) }.sumOf { it.testValue }
+fun part1(input: List<Equation>): Long = input.filter {
+    canBeCombined(it) { previous ->
+        when (previous) {
+            Operator.Plus -> Operator.Times
+            Operator.Times -> null
+            Operator.Concat -> TODO()
+        }
+    }
+}.sumOf { it.testValue }
 
-private fun canBeCombined(equation: Equation): Boolean {
+fun part2(input: List<Equation>): Long = input.filter {
+    canBeCombined(it) { previous ->
+        when (previous) {
+            Operator.Plus -> Operator.Times
+            Operator.Times -> Operator.Concat
+            Operator.Concat -> null
+        }
+    }
+}.sumOf { it.testValue }
+
+private fun canBeCombined(equation: Equation, nextOperator: Function<Operator, Operator?>): Boolean {
     var combo = OperatorCombo((0..<equation.numbers.size).map { Operator.Plus })
 
     while (true) {
@@ -16,17 +36,20 @@ private fun canBeCombined(equation: Equation): Boolean {
             return true
         }
 
-        combo = combo.nextCombo() ?: return false
+        combo = combo.nextComboUsing(nextOperator) ?: return false
     }
 }
 
 private class OperatorCombo(val operators: List<Operator>) {
-    fun nextCombo(): OperatorCombo? {
+
+    fun nextComboUsing(nextOperator: Function<Operator, Operator?>): OperatorCombo? {
         val newOperators = operators.toMutableList()
         for (index in operators.indices.reversed()) {
             val operator = operators[index]
-            if (operator == Operator.Plus) {
-                newOperators[index] = Operator.Times
+            val newOperator = nextOperator.apply(operator)
+
+            if (newOperator != null) {
+                newOperators[index] = newOperator
                 return OperatorCombo(newOperators.mapIndexed { i, o -> if (i > index) Operator.Plus else o })
             }
         }
@@ -42,6 +65,7 @@ private fun Equation.satisfiedBy(operatorCombo: OperatorCombo): Boolean {
         when (remainingOperators.removeFirst()) {
             Operator.Plus -> total += number
             Operator.Times -> total *= number
+            Operator.Concat -> total = "${total}${number}".toLong()
             else -> TODO()
         }
     }
--- src/test/kotlin/twenty_four/day7/Day7Test.kt
+++ src/test/kotlin/twenty_four/day7/Day7Test.kt
@@ -18,6 +18,16 @@ class Day7Test {
         assertThat(part1(parser.parse(shortData()))).isEqualTo(3749)
     }
 
+    @Test
+    fun `part 2 long`() {
+        assertThat(part2(parser.parse(data()))).isEqualTo(145149066755184)
+    }
+
+    @Test
+    fun `part 2 short`() {
+        assertThat(part2(parser.parse(shortData()))).isEqualTo(11387)
+    }
+
     private fun data(modifier: String? = null) = loadData(2024, 7, modifier)
 
     private fun shortData() = data("short")
