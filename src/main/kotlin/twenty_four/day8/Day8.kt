6a1368492e90e24bf75e6c8a9607872889578c30
Josh Jones <ohblonddev@gmail.com>
2024-12-21 19:39:00 +0000

    Solve day 8 part 2

--- src/main/kotlin/twenty_four/day8/Day8.kt
+++ src/main/kotlin/twenty_four/day8/Day8.kt
@@ -12,17 +12,55 @@ fun part1(map: Grid2D<Frequency?>): Int = map.mapIndexedNotNull { point, value -
         if (v != value || p == point) {
             null
         } else {
-            antinodePositions(map, point, p)
+            antinodePositionsWithResonanceMultiplier(map, point, p, 1)
         }
     }.flatten().flatten()
 }.flatten().flatten().toSet().count()
 
-private fun antinodePositions(map: Grid2D<Frequency?>, first: Grid2DPoint, second: Grid2DPoint): List<Grid2DPoint> {
+fun part2(map: Grid2D<Frequency?>): Int = map.mapIndexedNotNull { point, value ->
+    if (value == null) return@mapIndexedNotNull null
+
+    map.mapIndexedNotNull { p, v ->
+        if (v != value || p == point) {
+            null
+        } else {
+            allResonantAntinodePositions(map, point, p)
+        }
+    }.flatten().flatten()
+}.flatten().flatten().toSet().count()
+
+private fun allResonantAntinodePositions(
+    map: Grid2D<Frequency?>,
+    first: Grid2DPoint,
+    second: Grid2DPoint
+): List<Grid2DPoint> {
+    val positions: MutableSet<Grid2DPoint> = mutableSetOf(first, second)
+    var positionOffsetMultiplier = 1
+
+    while (true) {
+        val newPositions = antinodePositionsWithResonanceMultiplier(map, first, second, positionOffsetMultiplier)
+        if (newPositions.isEmpty()) break
+        positions.addAll(newPositions)
+        positionOffsetMultiplier += 1
+    }
+
+    return positions.toList()
+}
+
+private fun antinodePositionsWithResonanceMultiplier(
+    map: Grid2D<Frequency?>,
+    first: Grid2DPoint,
+    second: Grid2DPoint,
+    resonanceOffsetMultiplier: Int
+): List<Grid2DPoint> =
+    listOf(
+        nextResonantLocation(first, second, resonanceOffsetMultiplier),
+        nextResonantLocation(second, first, resonanceOffsetMultiplier),
+    ).filter { map.contains(it) }
+
+private fun nextResonantLocation(first: Grid2DPoint, second: Grid2DPoint, offsetMultiplier: Int): Grid2DPoint {
     val rowDiff = (first.row - second.row)
     val colDiff = (first.column - second.column)
 
-    return listOf(
-        Grid2DPoint(first.row + rowDiff, first.column + colDiff),
-        Grid2DPoint(second.row - rowDiff, second.column - colDiff)
-    ).filter { map.contains(it) }
+    return Grid2DPoint(first.row + rowDiff * offsetMultiplier, first.column + colDiff * offsetMultiplier)
 }
--- src/test/kotlin/twenty_four/day8/Day8Test.kt
+++ src/test/kotlin/twenty_four/day8/Day8Test.kt
@@ -18,6 +18,16 @@ class Day8Test {
         assertThat(part1(parser.parse(shortData()))).isEqualTo(14)
     }
 
+    @Test
+    fun `part 2 long`() {
+        assertThat(part2(parser.parse(data()))).isEqualTo(1015)
+    }
+
+    @Test
+    fun `part 2 short`() {
+        assertThat(part2(parser.parse(shortData()))).isEqualTo(34)
+    }
+
     private fun data(modifier: String? = null) = loadData(2024, 8, modifier)
 
     private fun shortData() = data("short")
