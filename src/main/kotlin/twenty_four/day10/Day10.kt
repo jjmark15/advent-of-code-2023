86eee9d8e5ea23fec1bb7d3939e4be22931ca331
Josh Jones <ohblonddev@gmail.com>
2024-12-22 16:24:32 +0000

    Refactor

--- src/main/kotlin/twenty_four/day10/Day10.kt
+++ src/main/kotlin/twenty_four/day10/Day10.kt
@@ -2,57 +2,45 @@ package twenty_four.day10
 
 import utils.Grid2D
 import utils.Grid2DPoint
-import java.util.function.Function
+import java.util.function.BiFunction
 import java.util.function.Predicate
 
-fun part1(map: Grid2D<Int>): Int {
-    val mapTraverser =
-        MapTraverser(map) { from -> map.orthogonalNeighbours(from).filter { map.get(it) == map.get(from) + 1 } }
-    return map.elementsMatching { height -> height == 0 }
-        .sumOf { trailhead -> mapTraverser.findReachableDestinations(trailhead) { it == 9 }.count() }
-}
+fun part1(input: Grid2D<Int>): Int = solve(input, true)
 
-fun part2(map: Grid2D<Int>): Int {
-    val mapTraverser =
-        MapTraverser(map) { from -> map.orthogonalNeighbours(from).filter { map.get(it) == map.get(from) + 1 } }
-    return map.elementsMatching { height -> height == 0 }
-        .sumOf { trailhead -> mapTraverser.countDistinctPathsToDestinations(trailhead) { it == 9 } }
+fun part2(input: Grid2D<Int>): Int = solve(input, false)
+
+private fun solve(input: Grid2D<Int>, uniquePaths: Boolean): Int {
+    val mapTraverser = MapTraverser(input) { map, from ->
+        input.orthogonalNeighbours(from).filter { map.get(it) == map.get(from) + 1 }
+    }
+    val trailheads = input.elementsMatching { height -> height == 0 }
+    return trailheads.sumOf { trailhead ->
+            mapTraverser.breadthFirstSearch(
+                trailhead,
+                { it: Int -> it == 9 },
+                uniquePaths
+            ).count()
+        }
 }
 
 private class MapTraverser<T>(
-    private val map: Grid2D<T>, private val nextPointFactory: Function<Grid2DPoint, List<Grid2DPoint>>
+    private val map: Grid2D<T>, private val nextPointFactory: BiFunction<Grid2D<T>, Grid2DPoint, List<Grid2DPoint>>
 ) {
-    fun findReachableDestinations(from: Grid2DPoint, destinationMatcher: Predicate<T>): List<Grid2DPoint> {
+    fun breadthFirstSearch(
+        from: Grid2DPoint, destinationMatcher: Predicate<T>, uniquePaths: Boolean = true
+    ): List<Grid2DPoint> {
         val visitedPlaces = mutableSetOf<Grid2DPoint>()
-        val placesToVisit = mutableListOf<Grid2DPoint>(from)
-        val reachableDestinations = mutableSetOf<Grid2DPoint>()
-
-        while (placesToVisit.isNotEmpty()) {
-            val place = placesToVisit.removeLast()
-            val newPlaces = nextPointFactory.apply(place)
-            visitedPlaces.add(place)
-            placesToVisit.addAll(newPlaces.filterNot { visitedPlaces.contains(it) })
-            if (destinationMatcher.test(map.get(place))) {
-                reachableDestinations.add(place)
-            }
-        }
-
-        return reachableDestinations.toList()
-    }
-
-    fun countDistinctPathsToDestinations(from: Grid2DPoint, destinationMatcher: Predicate<T>): Int {
         val placesToVisit = mutableListOf<Grid2DPoint>(from)
         val reachableDestinations = mutableListOf<Grid2DPoint>()
 
         while (placesToVisit.isNotEmpty()) {
             val place = placesToVisit.removeLast()
-            val newPlaces = nextPointFactory.apply(place)
-            placesToVisit.addAll(newPlaces)
-            if (destinationMatcher.test(map.get(place))) {
-                reachableDestinations.add(place)
-            }
+            val newPlaces = nextPointFactory.apply(map, place)
+            if (uniquePaths) visitedPlaces.add(place)
+            placesToVisit.addAll(newPlaces.filterNot { visitedPlaces.contains(it) })
+            if (destinationMatcher.test(map.get(place))) reachableDestinations.add(place)
         }
 
-        return reachableDestinations.count()
+        return reachableDestinations.toList()
     }
 }
