aa3faa86f248371f6a4a60a1a36c964c8d7b4e4c
Josh Jones <ohblonddev@gmail.com>
2024-12-23 20:31:21 +0000

    Minor refactor

--- src/main/kotlin/twenty_four/day10/Day10.kt
+++ src/main/kotlin/twenty_four/day10/Day10.kt
@@ -9,7 +9,7 @@ fun part2(input: Grid2D<Int>): Int = solve(input, false)
 
 private fun solve(input: Grid2D<Int>, uniquePaths: Boolean): Int {
     val grid2DTraverser = Grid2DTraverser(input) { map, from ->
-        input.cardinalNeighbours(from).filter { map.get(it) == map.get(from) + 1 }
+        map.cardinalNeighbours(from).filter { map.get(it) == map.get(from) + 1 }
     }
     val trailheads = input.elementsMatching { height -> height == 0 }
     return trailheads.sumOf { trailhead ->
