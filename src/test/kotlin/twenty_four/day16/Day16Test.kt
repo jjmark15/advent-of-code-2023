4f3a9ab0dab620b5e8c996699271587bbf7c568e
Josh Jones <ohblonddev@gmail.com>
2024-12-26 22:15:15 +0000

    Minor refactor

--- src/test/kotlin/twenty_four/day16/Day16Test.kt
+++ src/test/kotlin/twenty_four/day16/Day16Test.kt
@@ -9,23 +9,23 @@ import utils.grids.twodee.Grid2D
 
 class Day16Test : AbstractSolutionTest<Grid2D<MazeElement>>(2024, 16) {
     @Test
-    fun `part 1 long`() {
-        assertThat(part1(parser.parse(data()))).isEqualTo(65436)
-    }
-
-    @Test
     fun `part 1 short`() {
         assertThat(part1(parser.parse(shortData()))).isEqualTo(7036)
     }
 
     @Test
-    fun `part 2 long`() {
-        assertThat(part2(parser.parse(data()))).isEqualTo(489)
+    fun `part 1 long`() {
+        assertThat(part1(parser.parse(data()))).isEqualTo(65436)
     }
 
     @Test
     fun `part 2 short`() {
         assertThat(part2(parser.parse(shortData()))).isEqualTo(45)
+    }
+
+    @Test
+    fun `part 2 long`() {
+        assertThat(part2(parser.parse(data()))).isEqualTo(489)
     }
 
     override fun parser(): InputParser<Grid2D<MazeElement>> = InputParserImpl()
