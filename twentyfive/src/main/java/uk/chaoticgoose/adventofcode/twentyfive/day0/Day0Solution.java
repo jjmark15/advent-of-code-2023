28f16e27ef700fd270ede81a80ce78461bef8daa
Josh Jones <dev@chaoticgoose.uk>
2025-11-13 17:28:36 +0000

    Minor refactor

--- twentyfive/src/main/java/uk/chaoticgoose/adventofcode/twentyfive/day0/Day0Solution.java
+++ twentyfive/src/main/java/uk/chaoticgoose/adventofcode/twentyfive/day0/Day0Solution.java
@@ -10,10 +10,8 @@ import static java.util.Objects.requireNonNull;
 @NullMarked
 class Day0Solution {
     long part1(LocationIds input) {
-        var first = input.left.stream().sorted().toList();
-        var second = input.right.stream().sorted().toList();
-        return first.stream()
-            .gather(zipWith(second))
+        return input.left.stream().sorted()
+            .gather(zipWith(input.right.stream().sorted()))
             .mapToLong(e -> Math.abs(requireNonNull(e.first()) - requireNonNull(e.second())))
             .sum();
     }
