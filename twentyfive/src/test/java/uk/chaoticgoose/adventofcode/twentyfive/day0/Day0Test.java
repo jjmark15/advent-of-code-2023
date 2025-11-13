efb8f2df8197879b17159e9ec818f23ba21d1c75
Josh Jones <dev@chaoticgoose.uk>
2025-11-13 20:42:54 +0000

    Add long data example

--- twentyfive/src/test/java/uk/chaoticgoose/adventofcode/twentyfive/day0/Day0Test.java
+++ twentyfive/src/test/java/uk/chaoticgoose/adventofcode/twentyfive/day0/Day0Test.java
@@ -2,10 +2,12 @@ package uk.chaoticgoose.adventofcode.twentyfive.day0;
 
 import org.junit.jupiter.api.Test;
 import uk.chaoticgoose.adventofcode.twentyfive.utils.TestInputLoader;
+import uk.chaoticgoose.adventofcode.twentyfive.utils.TestInputLoader.InputDataModifier;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.chaoticgoose.adventofcode.twentyfive.utils.TestInputLoader.InputDataModifier.LONG;
 import static uk.chaoticgoose.adventofcode.twentyfive.utils.TestInputLoader.InputDataModifier.SHORT;
 
 class Day0Test {
@@ -14,11 +16,16 @@ class Day0Test {
     private final Day0Solution underTest = new Day0Solution();
 
     @Test
-    void part1() {
-        List<String> lines = testInputLoader.load(2025, 0, SHORT);
+    void part1Short() {
+        assertThat(underTest.part1(inputParser.parse(data(SHORT)))).isEqualTo(11L);
+    }
 
-        long result = underTest.part1(inputParser.parse(lines));
+    @Test
+    void part1Long() {
+        assertThat(underTest.part1(inputParser.parse(data(LONG)))).isEqualTo(2430334L);
+    }
 
-        assertThat(result).isEqualTo(11L);
+    private List<String> data(InputDataModifier inputDataModifier) {
+        return testInputLoader.load(2025, 0, inputDataModifier);
     }
 }
