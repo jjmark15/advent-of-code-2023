38c8a8dacca3c1dc1e5d3ce1aca1119221133b34
Josh Jones <dev@chaoticgoose.uk>
2025-11-15 09:51:01 +0000

    Update nullchecking dependencies

--- twentyfive/src/main/java/uk/chaoticgoose/adventofcode/twentyfive/day0/Day0Solution.java
+++ twentyfive/src/main/java/uk/chaoticgoose/adventofcode/twentyfive/day0/Day0Solution.java
@@ -1,6 +1,7 @@
 package uk.chaoticgoose.adventofcode.twentyfive.day0;
 
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 import uk.chaoticgoose.adventofcode.utils.Pair;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import static java.util.Objects.requireNonNull;
 
 @NullMarked
 class Day0Solution {
-    long part1(Pair<List<Long>> input) {
+    long part1(Pair<@Nullable List<Long>> input) {
         return input.left().stream().sorted()
             .gather(zipWith(input.right().stream().sorted()))
             .mapToLong(e -> Math.abs(requireNonNull(e.first()) - requireNonNull(e.second())))
