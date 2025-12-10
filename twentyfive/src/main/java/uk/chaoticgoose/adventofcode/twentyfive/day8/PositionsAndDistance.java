27b07d64e6743fcbfbe16989c3368692b7ae4c74
Josh Jones <dev@chaoticgoose.uk>
2025-12-10 18:43:25 +0000

    Remove public

--- twentyfive/src/main/java/uk/chaoticgoose/adventofcode/twentyfive/day8/PositionsAndDistance.java
+++ twentyfive/src/main/java/uk/chaoticgoose/adventofcode/twentyfive/day8/PositionsAndDistance.java
@@ -2,7 +2,7 @@ package uk.chaoticgoose.adventofcode.twentyfive.day8;
 
 import static java.util.Objects.requireNonNull;
 
-public record PositionsAndDistance(Position3D start, Position3D end, long distance) {
+record PositionsAndDistance(Position3D start, Position3D end, long distance) {
     public PositionsAndDistance {
         requireNonNull(start);
         requireNonNull(end);
